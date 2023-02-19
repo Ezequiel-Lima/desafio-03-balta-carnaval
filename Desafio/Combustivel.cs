24ec0617c6c1e169055fdab9f20de29ebdecc7c4
Ezequiel Lima <joao.lima94@fatec.sp.gov.br>
2023-02-19 09:17:51 -0300

    Add project files.

---
+++ Desafio/Combustivel.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace Desafio
+{
+    public class Combustivel
+    {
+        public Combustivel(double valorGasolina, double valorAlcool)
+        {
+            Validar(valorGasolina, valorAlcool);
+            ValorGasolina = valorGasolina;
+            ValorAlcool = valorAlcool;
+        }      
+
+        public double ValorGasolina { get; private set; }
+        public double ValorAlcool { get; private set; }
+
+        public double RelacaoPreco()
+        {
+            return Math.Round(ValorAlcool / ValorGasolina, 2) * 100;
+        }
+
+        private string ObterMelhorOpcao()
+        {
+            return RelacaoPreco() <= 72 ? "Álcool" : "Gasolina";
+        }
+
+        private void Validar(double valorGasolina, double valorAlcool)
+        {
+            if (valorGasolina <= 0 || valorAlcool <= 0)
+                throw new ArgumentException("Os valores de combustível devem ser positivos");
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"A relação de preço é de ({RelacaoPreco()}%)");
+            stringBuilder.AppendLine($"garante uma vantagem econômica para o motorista que escolher {ObterMelhorOpcao()} para abastecer o tanque do seu veículo.");
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ Desafio/Program.cs
@@ -0,0 +1,11 @@
+﻿using Desafio;
+
+Console.WriteLine("Informe o valor do litro do álcool:");
+double valorAlcool = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe o valor do litro da gasolina:");
+double valorGasolina = double.Parse(Console.ReadLine());
+
+Combustivel combustivel = new Combustivel(valorGasolina, valorAlcool);
+
+Console.WriteLine($"\n{combustivel}");
